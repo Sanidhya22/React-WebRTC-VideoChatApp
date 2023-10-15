71ea8a77bf486aca04d88b8fdced263faba4b983
Saniddhya Nigam <saniddhya_nigam@epam.com>
2023-10-15 16:41:01 +0530

    feature:peer-service

--- client/src/screens/room.jsx
+++ client/src/screens/room.jsx
@@ -1,13 +1,22 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { useSocket } from "../context/SocketProvider";
-import { useEffect } from "react";
+import ReactPlayer from "react-player";
+
 const RoomPage = () => {
   const socket = useSocket();
   const [remoteSocketId, setRemoteSocketId] = useState(null);
+  const [myStream, setMyStream] = useState(null);
   const handelUserJoined = useCallback(({ email, id }) => {
     console.log(email);
     setRemoteSocketId(id);
   });
+  const handelCallUser = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    setMyStream(stream);
+  }, []);
   useEffect(
     () => {
       socket.on("user:joined", handelUserJoined);
@@ -22,6 +31,16 @@ const RoomPage = () => {
     <>
       <div>Room</div>
       <h4>{remoteSocketId ? "Connected" : "No One In The Room"}</h4>
+      {remoteSocketId && <button onClick={handelCallUser}>Call</button>}
+      {myStream && (
+        <ReactPlayer
+          playing
+          muted
+          height="300px"
+          width="300px"
+          url={myStream}
+        ></ReactPlayer>
+      )}
     </>
   );
 };
